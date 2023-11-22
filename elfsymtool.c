74c8ee60a9856238ca1b8f0eb4b035753087b414
Jonathan Appavoo <jappavoo@bu.edu>
2023-11-22 04:27:57 +0000

    added initial parsing and validation of elf header

--- elfsymtool.c
+++ elfsymtool.c
@@ -148,8 +148,129 @@ int openElf(ELF *elf, char *path, int openflgs, int persist)
   
 }
 
+#define xstr(s) str(s)
+#define str(s) #s
+
+struct Desc {
+  int val;
+  char name[40];
+  char desc[80];
+};
+
+
+#define DESC(v, d) { \
+    .val = v, \
+      .name = #v,				\
+    .desc = d \
+      }
+
+struct Desc ETYPE[] = {
+  DESC(ET_NONE, "An unknown type"),
+  DESC(ET_REL, "A relocatable file"),
+  DESC(ET_EXEC, "An executable file"),
+  DESC(ET_DYN, "A shared object"),
+  DESC(ET_CORE, "A core file"),
+  DESC(-1, "")
+};
+
+struct Desc EICLASS[] = {
+  DESC(ELFCLASSNONE, "This class is invalid"),
+  DESC(ELFCLASS32, "32-bit architecture"),
+  DESC(ELFCLASS64, "64-bit architecture"),
+  DESC(-1, "")
+};
+
+struct Desc EIDATA[] = {
+  DESC(ELFDATANONE, "Unknown data format."),
+  DESC(ELFDATA2LSB, "Two's complement, little-endian."),
+  DESC(ELFDATA2MSB, "Two's complement, big-endian."),
+  DESC(-1,"")
+};
+
+struct Desc EIVERSION[] = {
+  DESC(EV_NONE, "Invalid version."),
+  DESC(EV_CURRENT, "Current version."),
+  DESC(-1,"")
+};
+
+struct Desc EIOSABI[] = {
+  DESC(ELFOSABI_SYSV, "UNIX System V ABI"),
+  DESC(ELFOSABI_HPUX, "HP-UX ABI"),
+  DESC(ELFOSABI_NETBSD, "NetBSD ABI"),
+  DESC(ELFOSABI_LINUX, "Linux ABI"),
+  DESC(ELFOSABI_SOLARIS, "Solaris ABI"),
+  DESC(ELFOSABI_IRIX, "IRIX ABI"),
+  DESC(ELFOSABI_FREEBSD, "FreeBSD ABI"),
+  DESC(ELFOSABI_TRU64, "TRU64 UNIX ABI"),
+  DESC(ELFOSABI_ARM, "ARM architecture ABI"),
+  DESC(ELFOSABI_STANDALONE, "Stand-alone  (embedded)"),
+  DESC(-1,"")
+};
+
+
+union EIDENT {
+  uint8_t raw[EI_NIDENT];
+  struct {
+    uint8_t m0;
+    uint8_t m1;
+    uint8_t m2;
+    uint8_t m3;
+    uint8_t class;
+    uint8_t data;
+    uint8_t version;
+    uint8_t osabi;
+    uint8_t abiversion;
+  } values;
+};
+      
+  
+void 
+printDesc(struct Desc *dtbl, int v, char *name, FILE *fp)
+{
+  for (int i=0; dtbl[i].val != -1; i++) {
+    if (dtbl[i].val == v) {
+      fprintf(fp, "%s : %d - %s : %s\n", name,
+	      dtbl[i].val, dtbl[i].name, dtbl[i].desc);
+      return;
+    }
+  }
+  assert(0);
+}
+
+void 
+printIdent(union EIDENT *ident, FILE *fp) {
+  fprintf(fp, "ident:\n");
+  fprintf(fp, "\tmagic[0]:0x%02hhx magic[1]:0x%02hhx"
+	  " magic[2]:0x%02hhx magic[3]:0x%02hhx\n",
+	  ident->values.m0, ident->values.m1,
+	  ident->values.m2, ident->values.m3);
+  printDesc(EICLASS, ident->values.class, "\tclass", fp);
+  printDesc(EIDATA, ident->values.data, "\tdata", fp);
+  printDesc(EIVERSION, ident->values.version, "\tversion", fp);
+  printDesc(EIOSABI, ident->values.osabi, "\tosabi", fp);
+  fprintf(fp, "\tabiversion: %d\n", ident->values.abiversion);
+}
+
+#if 0
+dumptype
+#endif
+    
+void printHdr(Elf64_Ehdr *hdr, FILE *fp)
+{
+  printIdent((union EIDENT *)&(hdr->e_ident), fp);
+}
+
+bool validElfIdent(ELF *elf) {
+  Elf64_Ehdr *hdr = (void *)(elf->addr);
+  union EIDENT *ident = (void *)&(hdr->e_ident);
+  return (ident->values.m0 == ELFMAG0 &&
+	  ident->values.m1 == ELFMAG1 &&
+	  ident->values.m2 == ELFMAG2 &&
+	  ident->values.m3 == ELFMAG3 );
+}
 int findStrTbl(ELF *elf)
 {
+  assert(elf);
   return 1;
 }
 
@@ -170,6 +291,16 @@ int main(int argc, char **argv)
 	       O_RDONLY, //open flags,
 	       0        // persist flg
 	       )) {
+    return -1;
+  }
+
+  if (verbose()) {
+    Elf64_Ehdr *hdr = (void *)elf.addr;
+    printHdr(hdr,stderr);
+  }
+
+  if (!validElfIdent(&elf)) {
+    fprintf(stderr, "ERROR: does not seem to be an elf file bad ident\n");
     return -1;
   }
   
